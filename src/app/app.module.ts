6147c78faa96a544072db27aaafe4c8f3f291cd6
yangxq5858 <yangxq5858@qq.com>
2018-05-26 19:58:40 +0800

    2

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+//这个根模块会告诉 Angular 如何组装该应用
+//引入模块
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
