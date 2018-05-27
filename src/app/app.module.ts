10e27fa7a6de2d7919d6fedfc8b2ba976f1268eb
yangxq5858 <yangxq5858@qq.com>
2018-05-27 11:02:55 +0800

    绑定事件，双向数据绑定

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; //双向数据绑定引入 表单模块
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NewsComponent } from './components/news/news.component';
@@ -14,7 +16,8 @@ import { HomeComponent } from './components/home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule  //当前的项目依赖哪些模块
+    BrowserModule,  //当前的项目依赖哪些模块
+    FormsModule     //双向数据绑定引入
   ],
   providers: [],   //定义的服务
   bootstrap: [AppComponent] //默认启动那个组件
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,19 +9,40 @@ export class HomeComponent implements OnInit {
 
   public title:String;
   public msg:any;
+  public username:String;
+  public flag = true;
+  public searchVal = '';
   constructor() {
     this.title = '这是Home组件';
     this.msg = `hi${this.title}
     
     haha  `;
+    this.username = '张三';
 
    }
 
   ngOnInit() {
   }
 
-   getMsg(){
+  getMsg(){
     alert(this.msg);
   }
 
+  setUserName(){
+    this.username = '李四';
+  }
+
+
+  //e 表示事件对象，在html哪儿传入 $event
+  //<input type="text" (keyup)='keyupFn($event)'>
+  keyupFn(e){
+    console.info(e);
+    if (e.keyCode === 13){
+      alert('你按了回车键了');
+    }
+  }
+
+  getSearchVal(){
+    alert(this.searchVal);
+  }
 }
