619d60e8144619820c20acb0c2c2bb444f7ff33e
yangxq5858 <yangxq5858@qq.com>
2018-05-27 12:39:20 +0800

    增加todoList 示例

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodolistComponent implements OnInit {
 
+  public todoVal:String = '';
+  public todoList = [];
+  public list1:Array<Object>;
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  addData(e){
+   
+    if (e.keyCode == 13){
+
+      //给数组加入一个对象
+      this.todoList.push({
+        todoVal:this.todoVal,
+        status:0
+      });
+
+      //双向数据绑定了的，这里加入到数组后，清空输入框中的值
+      this.todoVal = '';
+
+    }
+  }
+
+  delData(key){
+    //从数组中删除一个对象
+    this.todoList.splice(key,1);
+    
+  }
+
+  changeStatus(key,val){
+    this.todoList[key].status = val;
   }
 
 }
