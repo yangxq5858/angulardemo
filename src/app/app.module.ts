34129bae5534bbe12684ac8ad7fe8c2d74acd0af
yangxq5858 <yangxq5858@qq.com>
2018-05-27 10:09:27 +0800

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,24 @@
-//这个根模块会告诉 Angular 如何组装该应用
-//引入模块
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-//引入自定义模块
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NewsComponent } from './components/news/news.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  //引入当前项目运行的的组件  自定义组件都需要引入并且在这个里面配置
-  declarations: [
+  declarations: [          //
     AppComponent,
     HeaderComponent,
-    NewsComponent
+    NewsComponent,
+    HomeComponent
   ],
-  //当前的项目依赖哪些模块
   imports: [
-    BrowserModule  
+    BrowserModule  //
   ],
-  //定义的服务
-  providers: [], 
-  //默认启动那个组件
-  bootstrap: [AppComponent] 
+  providers: [],   //
+  bootstrap: [AppComponent] //
 })
-//根模块不需要导出任何东西，   因为其它组件不需要导入根模块。 但是一定要写
-export class AppModule { }
+export class AppModule { } // 
 
 
 
